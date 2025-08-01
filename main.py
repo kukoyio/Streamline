4f13b1db32e1b65a35019dd150e04cd0598d60ce
Ayo Kukoyi <kukoyio@tcd.ie>
2025-08-01 10:26:36 +0100

    Added flask import to connect backend to frontend and Restructured folder accordingly

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+from flask import Flask, render_template, url_for
 from dotenv import load_dotenv
 import os
 import base64
 from requests import post, get
 import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/spotlight')
+def artist_spotlight():
+    return render_template("artist-spotlight.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 load_dotenv()
 
